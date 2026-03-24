c8c1533b4c2e130cfed87351a9d8300dc2f0c550
Caio Fabrini <caiofabrini@outlook.com>
2026-03-23 23:11:36 -0300

    criando projeto com cocoapods e SPM

---
+++ ProjetoCocoapods/ProjetoCocoapods/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjetoCocoapods
+//
+//  Created by Caio Fabrini on 23/03/26.
+//
+
+import UIKit
+import Charts
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+  }
+
+
+}
+
