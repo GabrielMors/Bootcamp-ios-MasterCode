792cc15d174a43d88dc0ba16f72f62da223654ee
Caio Fabrini <caiofabrini@outlook.com>
2026-04-02 22:06:02 -0300

    criando toda a estrutura para codable, encodable, decodable, codingKeys

--- ProtocolsNetwork/ProtocolsNetwork/main.swift
+++ ProtocolsNetwork/ProtocolsNetwork/main.swift
@@ -104,49 +104,274 @@ var alunos: [Aluno] = [
 // 3 - Crie um JSON representando o objeto Veiculo fornecido em Swift
 
 struct Veiculo {
-    var id: Int
-    var marca: String
-    var modelo: String
-    var ano: Int
-    var acessorios: [Acessorio]
+  var id: Int
+  var marca: String
+  var modelo: String
+  var ano: Int
+  var acessorios: [Acessorio]
 }
 
 struct Acessorio {
-    var nome: String
-    var preco: Double
-    var configuracaoAcessorio: ConfiguracaoAcessorio
+  var nome: String
+  var preco: Double
+  var configuracaoAcessorio: ConfiguracaoAcessorio
 }
 
 struct ConfiguracaoAcessorio {
-    var tipo: String
-    var dataCriacao: String
+  var tipo: String
+  var dataCriacao: String
 }
 
 // Objeto com valores
 let veiculo = Veiculo(
-    id: 101,
-    marca: "Toyota",
-    modelo: "Corolla",
-    ano: 2022,
-    acessorios: [
-        Acessorio(
-            nome: "Teto solar",
-            preco: 2500.00,
-            configuracaoAcessorio: ConfiguracaoAcessorio(
-                tipo: "Luxo",
-                dataCriacao: "2021-06-15"
-            )
-        ),
-        Acessorio(
-            nome: "Multimídia",
-            preco: 1200.00,
-            configuracaoAcessorio: ConfiguracaoAcessorio(
-                tipo: "Tecnologia",
-                dataCriacao: "2021-08-20"
-            )
-        )
-    ]
+  id: 101,
+  marca: "Toyota",
+  modelo: "Corolla",
+  ano: 2022,
+  acessorios: [
+    Acessorio(
+      nome: "Teto solar",
+      preco: 2500.00,
+      configuracaoAcessorio: ConfiguracaoAcessorio(
+        tipo: "Luxo",
+        dataCriacao: "2021-06-15"
+      )
+    ),
+    Acessorio(
+      nome: "Multimídia",
+      preco: 1200.00,
+      configuracaoAcessorio: ConfiguracaoAcessorio(
+        tipo: "Tecnologia",
+        dataCriacao: "2021-08-20"
+      )
+    )
+  ]
 )
+
+// Correção:
+
+// Desafio 1
+
+// [
+//    {
+//        "nome": "Ana",
+//        "idade": 28
+//    },
+//    {
+//        "nome": "Ricardo",
+//        "idade": 35
+//    }
+// ]
+
+// Desafio 2
+
+// [
+//    {
+//        "id": "sdsdfvdf5445545",
+//        "nome": "Fogão",
+//        "preco": 105.45
+//    },
+//    {
+//        "id": "ggg54154545",
+//        "nome": "Geladeira",
+//        "preco": 1000.00
+//    },
+//    {
+//        "id": "54fertf452tfef5fe",
+//        "nome": "Microondas",
+//        "preco": 509.30
+//    }
+// ]
+
+struct Produto {
+  var id: String
+  var nome: String
+  var preco: Double
+}
+
+var produtos: [Produto] = [
+  Produto(id: "sdsdfvdf5445545", nome: "Fogão", preco: 105.45),
+  Produto(id: "ggg54154545", nome: "Geladeira", preco: 1000.00),
+  Produto(id: "54fertf452tfef5fe", nome: "Microondas", preco: 509.30)
+]
+
+// Desafio 3
+
+// {
+//    "id": 101,
+//    "marca": "Toyota",
+//    "modelo": "Corolla",
+//    "ano": 2022,
+//    "acessorios": [
+//        {
+//            "nome": "Teto Solar",
+//            "preco": 2500.00,
+//            "configuracaoAcessorio": {
+//                "tipo": "Luxo",
+//                "dataCriacao": "2021-06-15"
+//            }
+//        },
+//        {
+//            "nome": "Multimídia",
+//            "preco": 1200.00,
+//            "configuracaoAcessorio": {
+//                "tipo": "Tecnologia",
+//                "dataCriacao": "2021-08-20"
+//            }
+//        }
+//    ]
+// }
+
+// MARK: - Decodable
+
+// Decodable é um protocolo em Swift que permite que você converta dados no formato JSON em um objeto Swift. Este processo é conhecido como decodificação.
+
+// OBS: Quando tentar realizar o decode(parse) ele vai olhar sempre buscando a chave e a tipagem se ambos são IGUAIS! Caso não sejam iguais, ele cai no catch.
+// Tudo oque tem no MODELO SWIFT que não seja opcional, ele se torna algo OBRIGATORIO!! Caso o back não retorne aquele campo, ele vai cair no catch. Caso a variavel seja opcional, ai não tem problema, pois seu valor será nil
+
+struct House: Decodable {
+  var address: String
+  var numberOfRooms: Int
+  var hasGarden: Bool
+}
+
+let jsonString = """
+{
+  "address": "Avenida 4562 bairro jardim europa",
+  "numberOfRooms": 23435,
+  "hasGarden": false
+}
+"""
+
+//if let jsonData = jsonString.data(using: .utf8) {
+//  do {
+//    let house: House = try JSONDecoder().decode(House.self, from: jsonData)
+//    print("House Decodificado com sucesso!!!")
+//    print(house.address)
+//    print(house.numberOfRooms)
+//    print(house.hasGarden)
+//  } catch {
+//    // Ele tenta (try) decodificar, se caso não conseguir, ele cai no caso do catch (falha)
+//    print("Error ao decodificar o JSON \(error.localizedDescription)")
+//  }
+//}
+
+// MARK: - CodingKey
+
+struct Animal: Decodable {
+  var species: String
+  var age: Int
+  var habitatType: String
+  var isDangerous: Bool
+
+  enum CodingKeys: String, CodingKey {
+    case species
+    case age
+    case habitatType = "habitat_type"
+    case isDangerous = "is_dangerous"
+  }
+}
+
+let jsonStringAnimal = """
+{
+ "species" : "Tigre",
+ "age": 1,
+ "habitat_type": "Floresta",
+ "is_dangerous": true
+}
+"""
+
+//if let jsonDataAnimal = jsonStringAnimal.data(using: .utf8) {
+//  do {
+//    let animal: Animal = try JSONDecoder().decode(Animal.self, from: jsonDataAnimal)
+//    print("Animal Decodificado com sucesso!!!")
+//    print(animal.habitatType)
+//    print(animal.isDangerous)
+//    print(animal.species)
+//  } catch {
+//    // Ele tenta (try) decodificar, se caso não conseguir, ele cai no caso do catch (falha)
+//    print("Error ao decodificar o JSON \(error.localizedDescription)")
+//  }
+//}
+
+// MARK: Encodable
+
+// Encodable é outro protocolo em Swift. Ele permite que você faça o oposto do que o Decodable faz: você pode converter um objeto Swift em dados no formato JSON. Isso é chamado de codificação.
+
+struct Dog: Encodable {
+  var name: String
+  var age: Int
+  var breed: String
+  var isVaccinated: Bool
+
+  enum CodingKeys: String, CodingKey {
+    case name
+    case age
+    case breed
+    case isVaccinated = "is_vaccinated"
+  }
+}
+
+//let dog = Dog(name: "Max", age: 3, breed: "Labrador", isVaccinated: true)
+//
+//do {
+//  let encoder = JSONEncoder()
+//  encoder.outputFormatting = .prettyPrinted // facilita a leitura do json
+//  let jsonDataDog = try encoder.encode(dog)
+//  let jsonStringDog = String(data: jsonDataDog, encoding: .utf8) ?? "Erro ao converter para string"
+//  print(jsonStringDog)
+//} catch {
+//  print("Error ao codificar: \(error.localizedDescription)")
+//}
+
+
+// MARK: - Codable
+
+// O Codable ele é um Typealias que é a junção do Decodable + Encodable
+// Pois ele já é ambos ;)
+
+struct Carro: Codable {
+  let marca: String
+  let modelo: String
+  let ano: Int
+}
+
+let jsonStringCarro = """
+{
+ "marca": "Ferrari",
+ "modelo": "458 Pista",
+ "ano": 2018
+}
+"""
+
+if let jsonStringCarro = jsonStringCarro.data(using: .utf8) {
+  do {
+    let carro: Carro = try JSONDecoder().decode(Carro.self, from: jsonStringCarro)
+    print("Carro Decodificado com sucesso!!!")
+    print(carro.marca)
+    print(carro.modelo)
+    print(carro.ano)
+  } catch {
+    // Ele tenta (try) decodificar, se caso não conseguir, ele cai no caso do catch (falha)
+    print("Error ao decodificar o JSON \(error.localizedDescription)")
+  }
+}
+
+
+let car = Carro(marca: "Ferrari", modelo: "458 Pista", ano: 2018)
+
+do {
+  let encoder = JSONEncoder()
+  encoder.outputFormatting = .prettyPrinted // facilita a leitura do json
+  let jsonDataCar = try encoder.encode(car)
+  let jsonCar = String(data: jsonDataCar, encoding: .utf8) ?? "Erro ao converter para string"
+  print(jsonCar)
+} catch {
+  print("Error ao codificar: \(error.localizedDescription)")
+}
+
+
+
 
 
 
