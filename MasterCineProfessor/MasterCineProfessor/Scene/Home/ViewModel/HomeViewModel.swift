881311cbafcde79d92cd11fb7f5afd38e367dcea
Caio Fabrini <caiofabrini@outlook.com>
2026-04-27 21:26:19 -0300

    criando paginacao

--- MasterCineProfessor/MasterCineProfessor/Scene/Home/ViewModel/HomeViewModel.swift
+++ MasterCineProfessor/MasterCineProfessor/Scene/Home/ViewModel/HomeViewModel.swift
@@ -7,6 +7,10 @@
 
 import Foundation
 
+enum ShouldLoadNextPage: Int {
+   case page = 3
+}
+
 protocol HomeViewModelProtocol: AnyObject {
   func didUpdateMovies()
   func didChangeLoading(start: Bool)
@@ -20,36 +24,92 @@ final class HomeViewModel {
   private(set) var isError: Bool = false
   private var isInitialRequest: Bool = true
 
+  private var isLoadingPage: Bool = false
+  private var currentPage: Int = 1
+  private var totalPages: Int = 1
+  private var currentQuery: String = ""
+
+  private var hasMorePages: Bool {
+    return currentPage < totalPages
+  }
+
   func fetchPopularMovies() {
-    delegate?.didChangeLoading(start: true)
-    service.fetchPopular(page: 1) { [weak self] result in
+    currentQuery = ""
+    currentPage = 1
+    totalPages = 1
+    isError = false
+    fetchPopularMovies(page: currentPage, isPagination: false)
+  }
+
+  private func fetchPopularMovies(page: Int, isPagination: Bool) {
+    guard !isLoadingPage else { return }
+
+    isLoadingPage = true
+    if !isPagination {
+      delegate?.didChangeLoading(start: true)
+    }
+
+    service.fetchPopular(page: page) { [weak self] result in
       guard let self else { return }
+      isLoadingPage = false
       delegate?.didChangeLoading(start: false)
       switch result {
       case .success(let success):
-        movies = success.results
+        currentPage = success.page
+        totalPages = success.totalPages
+        if !isPagination {
+          movies = success.results
+        } else {
+          movies.append(contentsOf: success.results)
+        }
         isError = false
       case .failure:
         isError = true
-        movies.removeAll()
+        if !isPagination {
+          movies.removeAll()
+        }
       }
+
       isInitialRequest = false
       delegate?.didUpdateMovies()
     }
   }
 
   private func fetchSearch(query: String) {
-    delegate?.didChangeLoading(start: true)
-    service.search(query: query, page: 1) { [weak self] result in
+    currentPage = 1
+    totalPages = 1
+    isError = false
+    currentQuery = query
+    fetchSearch(query: query, page: currentPage, isPagination: false)
+  }
+
+  private func fetchSearch(query: String, page: Int, isPagination: Bool) {
+    guard !isLoadingPage else { return }
+
+    isLoadingPage = true
+    if !isPagination {
+      delegate?.didChangeLoading(start: true)
+    }
+
+    service.search(query: query, page: page) { [weak self] result in
       guard let self else { return }
+      isLoadingPage = false
       delegate?.didChangeLoading(start: false)
       switch result {
       case .success(let success):
-        movies = success.results
+        currentPage = success.page
+        totalPages = success.totalPages
+        if !isPagination {
+          movies = success.results
+        } else {
+          movies.append(contentsOf: success.results)
+        }
         isError = false
       case .failure:
         isError = true
-        movies.removeAll()
+        if !isPagination {
+          movies.removeAll()
+        }
       }
       delegate?.didUpdateMovies()
     }
@@ -75,5 +135,26 @@ final class HomeViewModel {
       return
     }
     fetchSearch(query: query)
+  }
+
+  func loadNextPageIfNeeded(index: Int) {
+    guard !isError,
+          !isEmptyMovie,
+          !isLoadingPage,
+          hasMorePages
+          else { return }
+
+    let lastIndex = movies.count - 1
+    let shouldLoadNextPage = index >= lastIndex - ShouldLoadNextPage.page.rawValue
+
+    guard shouldLoadNextPage else { return }
+
+    let nextPage = currentPage + 1
+
+    if currentQuery.isEmpty {
+      fetchPopularMovies(page: nextPage, isPagination: true)
+    } else {
+      fetchSearch(query: currentQuery, page: nextPage, isPagination: true)
+    }
   }
 }
